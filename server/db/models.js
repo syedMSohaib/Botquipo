56b871a31eb369f4d1588cace220004e5a7dd152
Travis A. Gibson <travisgibson.dev@gmail.com>
2020-07-29 13:56:04 +0500

    initial commit

--- server/db/models.js
+++ server/db/models.js
@@ -6,6 +6,7 @@ function getBotModels(req, res, next) {
   logger.winston.info('Model.getBotModels');
   db.all('select * from models where bot_id = ?  order by model_id desc', req.params.bot_id, function(err, data) {
     if (err) {
+      console.log('err', err);
       logger.winston.error(err);
     } else {
       res.status(200).json(data);
